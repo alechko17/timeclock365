f9a33372e5ec49969222fa44d92be5b4da4312d8
Alek Shpitalnik <alekshpitalnik@gmail.com>
2022-07-01 09:19:01 +0300

    add books and authors entities

---
+++ src/Form/BooksType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Books;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BooksType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('cover_image')
+            ->add('year_of_publication')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Books::class,
+        ]);
+    }
+}
